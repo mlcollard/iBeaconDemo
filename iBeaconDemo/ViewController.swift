cc249092d8152c7ab4bb6fb6366ce50ed5e4e40d
Michael L. Collard, Ph.D <collard@uakron.edu>
2018-05-04 08:36:27 -0400

    Initial Commit

---
+++ iBeaconDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBeaconDemo
+//
+//  Created by Michael Collard on 5/4/18.
+//  Copyright © 2018 Michael Collard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
