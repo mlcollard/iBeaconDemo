314339f8f3af218d244db63d5922af969bf42e77
Michael L. Collard, Ph.D <collard@uakron.edu>
2018-05-04 09:22:08 -0400

    Add basic setup from old demo

--- iBeaconDemo/ViewController.swift
+++ iBeaconDemo/ViewController.swift
@@ -7,19 +7,74 @@
 //
 
 import UIKit
+import CoreLocation
+import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        /*
+ NSLocationAlwaysAndWhenInUseUsageDescription
+         NSLocationWhenInUseUsageDescription
+         
+ */
+        let beaconIdentifier = "iBeaconModules.us"
+        
+        guard let beaconUUID = UUID(uuidString: "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6") else {
+            return
+        }
+        
+        let beaconRegion = CLBeaconRegion(proximityUUID: beaconUUID, identifier: beaconIdentifier)
+        
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        locationManager.pausesLocationUpdatesAutomatically = false
+        
+        locationManager.startMonitoring(for: beaconRegion)
+        locationManager.startRangingBeacons(in: beaconRegion)
+        locationManager.startUpdatingLocation()
+        
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) {
+            granted, error in
+
+            print(#line)
+            print(granted)
+
+            if let error = error {
+                print(error)
+                return
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func locationManager(didRangeBeacons beacons: [AnyObject],
+                         inRegion region: CLBeaconRegion!) {
+        print("didRangeBeacons");
+        var message = ""
+        
+        if (beacons.count > 0) {
+            let nearestBeacon:CLBeacon = beacons[0] as! CLBeacon
+            
+            switch nearestBeacon.proximity {
+            case .unknown:
+                message = "You are unknown"
+            case .immediate:
+                message = "You are in the immediate proximity of the beacon"
+            case .near:
+                message = "You are near the beacon"
+            case .far:
+                message = "You are far away from the beacon"
+            }
+        } else {
+            message = "No beacons are nearby"
+        }
+        
+        print(message)
     }
-
-
+    
 }
-
